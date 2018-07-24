447b85ed3ed60b4d5eba10446c980495127123e2
Chih-Wei Huang <cwhuang@linux.org.tw>
2018-07-24 16:54:48 +0800

    Remove radeonsi from the list of radeon_drivers
    The radeonsi uses the pipe driver.

--- Android.mk
+++ Android.mk
@@ -25,7 +25,7 @@ DRM_GPU_DRIVERS := $(strip $(filter-out swrast, $(BOARD_GPU_DRIVERS)))
 
 freedreno_drivers := freedreno
 intel_drivers := i915 i965 i915g ilo
-radeon_drivers := r300g r600g radeonsi
+radeon_drivers := r300g r600g
 nouveau_drivers := nouveau
 
 valid_drivers := \
